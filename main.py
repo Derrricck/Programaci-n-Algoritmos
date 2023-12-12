6cd02cca3dff67d40b234c8c1f7f1ebaf62b2090
Derrricck <151797763+Derrricck@users.noreply.github.com>
2023-12-12 11:03:14 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+# Este programa tiene como objetivo reforzar los conceptos básicos de Python que hemos cubierto hasta ahora.
+# Ejercicio 1: Operaciones Básicas y Tipos de Datos
+# Suma dos números enteros.
+a=5
+b=6
+c=(a+b)
+print(c)
+# Resta dos números de punto flotante.
+a=3.1
+b=2.2
+c=(a-b)
+print(c)
+# Multiplica dos números enteros.
+a=5
+b=3
+c=(a*b)
+print(c)
+# Divide dos números de punto flotante.
+a=10
+b=2
+c=(a/b)
+print(c)
+print("En esta parte se desarrollan operaciones como la suma, resta, multiplicacion y division.")
+# Ejercicio 2: Manipulación de Cadenas de Texto
+a=("Derrick Marcelo Guambaña Bravo")
+print(a)
+b=len(a)
+print(b)
+# Este parte del programa sirve para contar las letras de la linea a, en otras palabras tu nombre completo.
+# Ejercicio 3: Conversión de Tipos de Datos
+a=2
+b=1.5
+print(a)
+print(float(a))
+print(b)
+print(int(b))
+# En esta parte se transformo de float a entera y viceversa
